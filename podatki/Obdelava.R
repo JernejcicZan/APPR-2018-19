174fdd238d56266b31e486f794a747c67fb26b9f
Žan Jernejčič <zan.jernejcic@gmail.com>
2018-12-14 09:59:29 +0100

    pobrisani nepotrebni stolpci

--- podatki/Obdelava.R
+++ podatki/Obdelava.R
@@ -5,3 +5,11 @@ emi_po_starosti_in_spolu
 imig_po_spolu_in_starosti
 imigr_po_narodnosti
 #Podatki prebrani
+
+BDP_in_rast[4] <- NULL
+BDP_in_rast[5] <- NULL
+emi_next_destination[4:7] <- list(NULL)
+emi_next_destination[5] <- NULL
+emi_po_starosti_in_spolu[,c(3,4,6,9)] <- list(NULL)
+imig_po_spolu_in_starosti[,c(3,4,6,9)] <- list(NULL)
+imigr_po_narodnosti[,c(4,5,6,8)]<-list(NULL)
