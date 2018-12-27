81cd9dc434044ae64ee30026329cd83f44b8cacf
Žan Jernejčič <zan.jernejcic@gmail.com>
2018-12-27 09:57:24 +0100

    NIČ POSEBNEGA

--- podatki/Obdelava.R
+++ podatki/Obdelava.R
@@ -1,3 +1,7 @@
+library(ggplot2)
+library(dplyr)
+
+
 #Podatki uvoženi
 BDP_in_rast
 emi_next_destination
@@ -16,8 +20,13 @@ imig_po_spolu_in_starosti[,c(3,4,6,9)] <- list(NULL)
 imigr_po_narodnosti[,c(4,5,6,8)]<-list(NULL)
 
 #Preimenovanje stolpcev
-names(BDP_in_rast) <- c("Leto", "Država", "BDP/rast", "število")
+names(BDP_in_rast) <- c("Leto", "Država", "BDP_rast", "število")
 names(emi_next_destination) <- c("Leto", "Država", "Država izselitve", "število")
 names(emi_po_starosti_in_spolu) <- c("Leto", "Država","Starost", "Spol", "Število")
 names(imig_po_spolu_in_starosti) <- c("Leto", "Država", "Starost", "Spol", "število")
 names(imigr_po_narodnosti) <- c("Leto", "Država", "Državljanstvo", "število")
+
+#Risanje grafov
+# gdp <- filter(BDP_in_rast,BDP_rast == "Current prices, euro per capita")
+# View(gdp)
+# ggplot2()
