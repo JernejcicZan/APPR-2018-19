bd4d5ebf480fc081c9b180ff1f8feeb6f382a31e
Žan Jernejčič <zan.jernejcic@gmail.com>
2018-12-27 09:12:55 +0100

    majhna obdelava

--- podatki/Obdelava.R
+++ podatki/Obdelava.R
@@ -6,6 +6,7 @@ imig_po_spolu_in_starosti
 imigr_po_narodnosti
 #Podatki prebrani
 
+#Nepotrbni stolpci pobrisani
 BDP_in_rast[4] <- NULL
 BDP_in_rast[5] <- NULL
 emi_next_destination[4:7] <- list(NULL)
@@ -13,3 +14,10 @@ emi_next_destination[5] <- NULL
 emi_po_starosti_in_spolu[,c(3,4,6,9)] <- list(NULL)
 imig_po_spolu_in_starosti[,c(3,4,6,9)] <- list(NULL)
 imigr_po_narodnosti[,c(4,5,6,8)]<-list(NULL)
+
+#Preimenovanje stolpcev
+names(BDP_in_rast) <- c("Leto", "Država", "BDP/rast", "število")
+names(emi_next_destination) <- c("Leto", "Država", "Država izselitve", "število")
+names(emi_po_starosti_in_spolu) <- c("Leto", "Država","Starost", "Spol", "Število")
+names(imig_po_spolu_in_starosti) <- c("Leto", "Država", "Starost", "Spol", "število")
+names(imigr_po_narodnosti) <- c("Leto", "Država", "Državljanstvo", "število")
