631c6c23a296c360bd45c4b138c13e77edff58a9
Žan Jernejčič <zan.jernejcic@gmail.com>
2019-01-04 10:02:39 +0100

    malce sprememb

--- podatki/Obdelava.R
+++ podatki/Obdelava.R
@@ -33,5 +33,14 @@ names(BDP_in_rast) <- c("Leto", "Država", "Rast BDP", "BDP per capita")
 #v imig_po_spolu_starosti moramo pobrisat vrstice, kjer je starostena od
 #(Less than 15 years,From 15 to 64 years,From 65 to 69 years),enako pri emi_po_spolu
 
+imig_po_spolu_in_starosti <- subset(imig_po_spolu_in_starosti, Starost=="Less than 15 years" |
+        Starost=="From 15 to 64 years" | Starost=="65 years or over" | Starost=="Total"  )
+
+emi_po_starosti_in_spolu <- subset(emi_po_starosti_in_spolu, Starost=="Less than 15 years" |
+        Starost=="From 15 to 64 years" | Starost=="65 years or over" | Starost=="Total" )
+
+imigr_po_narodnosti <- imigr_po_narodnosti[!(imigr_po_narodnosti$Državljanstvo %in% c("Foreign country","Europe","Reporting country")),]
+
+
 
 
