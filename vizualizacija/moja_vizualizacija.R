e6ba1961b9f6080cb8f62115f47a1d4f00625a77
Žan Jernejčič <zan.jernejcic@gmail.com>
2019-01-08 11:44:37 +0100

    zemljevid

---
+++ vizualizacija/moja_vizualizacija.R
@@ -0,0 +1,25 @@
+library(rgdal)
+library(rgeos)
+library(mosaic)
+library(maptools)
+library(reshape2)
+library(ggplot2)
+library(munsell)
+
+# Uvozimo zemljevid Sveta
+# source("https://raw.githubusercontent.com/jaanos/APPR-2018-19/master/lib/uvozi.zemljevid.r")
+source("lib/uvozi.zemljevid.r") #Nastavi pravo datoteko
+
+zemljevid <- uvozi.zemljevid("https://www.naturalearthdata.com/http//www.naturalearthdata.com/download/50m/cultural/ne_50m_admin_0_countries.zip",
+                             "ne_50m_admin_0_countries") %>%
+  fortify()
+
+
+# Zemljevid sveta skrčimo na zemljevid Evrope
+Evropa <- filter(zemljevid, CONTINENT == "Europe")
+Evropa <- filter(Evropa, long < 55 & long > -45 & lat > 30 & lat < 85)
+
+ggplot(Evropa, aes(x=long, y=lat, group=group, fill=NAME)) + 
+  geom_polygon() + 
+  labs(title="Evropa - osnovna slika") +
+  theme(legend.position="none")
