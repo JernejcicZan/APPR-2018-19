6ed2d7b96806228c0d33c28eb37389781c14482d
Žan Jernejčič <zan.jernejcic@gmail.com>
2019-01-14 22:58:25 +0100

    Dokončal grafe

--- uvoz/mojuvoz.R
+++ uvoz/mojuvoz.R
@@ -8,6 +8,7 @@ BDP <- read_csv("podatki/BDP.csv", na=":")
 BDP <- BDP[c(-3,-4,-6)]
 names(BDP) <- c("Leto", "Država", "BDP_pc")
 BDP$Država <- gsub("^Germany.*", "Germany",BDP$Država)
+BDP <- BDP%>%filter(Leto!=2017)
 
 #Uvoz in ureditev tabele za izseljevanje
 emi_naslednja <- read_csv("podatki/emi_naslednja.csv", na=":")
@@ -17,11 +18,14 @@ emi_naslednja$Država <- gsub("^Germany.*", "Germany",emi_naslednja$Država)
 emi_naslednja$`Država izselitve` <- gsub("^Germany.*", "Germany",emi_naslednja$`Država izselitve`)
 
 
+
 #Uvoz in ureditev tabele za izseljevanje po starosti in spolu
 emi_starost_spol <- read_csv("podatki/emi_po_starosti_in_spolu.csv", na=":")
 emi_starost_spol <- emi_starost_spol[c(-3,-4,-6,-9)]
 names(emi_starost_spol) <- c("Leto", "Država","Starost", "Spol", "Izseljeni")
 emi_starost_spol$Država <- gsub("^Germany.*", "Germany",emi_starost_spol$Država)
+emi_starost_spol$Starost <- gsub("Less than 15 years", "14 years or less", emi_starost_spol$Starost)
+emi_starost_spol$Starost <- gsub("65 years or over", "Over 65 years", emi_starost_spol$Starost)
 
 
 #Uvoz in ureditev tabele za priseljevanje po starosti in spolu
@@ -31,7 +35,8 @@ names(imig_starost_spol) <- c("Leto", "Država", "Starost", "Spol", "Priseljeni"
 imig_starost_spol <- imig_starost_spol[!(imig_starost_spol$Država=="Former Yugoslav Republic of Macedonia, the"
 ),]
 imig_starost_spol$Država <- gsub("^Germany.*", "Germany",imig_starost_spol$Država)
-
+imig_starost_spol$Starost <- gsub("Less than 15 years", "14 years or less", imig_starost_spol$Starost)
+imig_starost_spol$Starost <- gsub("65 years or over", "Over 65 years", imig_starost_spol$Starost)
 
 
 
@@ -42,6 +47,11 @@ names(imig_po_narodnosti) <- c("Leto", "Država", "Državljanstvo", "število")
 imig_po_narodnosti$Država <- gsub("^Germany.*", "Germany",imig_po_narodnosti$Država)
 imig_po_narodnosti$Državljanstvo <- gsub("^Germany.*", "Germany",imig_po_narodnosti$Državljanstvo)
 imig_po_narodnosti <- imig_po_narodnosti[!(imig_po_narodnosti$Državljanstvo=="Reporting country"),]
+
+
+neto_tabela <- left_join(imig_starost_spol, emi_starost_spol)
+neto_tabela <- neto_tabela %>% mutate(Neto = Priseljeni - Izseljeni)
+neto_tabela[,c(5,6)] <- NULL
 
 
 #Moje tabele
--- vizualizacija/moja_vizualizacija.R
+++ vizualizacija/moja_vizualizacija.R
@@ -25,32 +25,60 @@ ggplot(Evropa, aes(x=long, y=lat, group=group, fill=NAME)) +
 
 
 najvecji.bdp <- BDP %>% filter(Leto == 2011) %>%
-  top_n(10, BDP_pc)
+  top_n(6, BDP_pc)
 
-najnizji.bdp <- BDP %>% filter(Leto==2011) %>% top_n(10,-1*BDP_pc)
+najnizji.bdp <- BDP %>% filter(Leto==2011) %>% top_n(6,-1*BDP_pc)
 
-#Graf za 10 najbolj razvitih držav
+#Graf za 6 najbolj razvitih držav
 graf_BDP_najvecji <- ggplot(data = BDP %>% filter(Država %in% najvecji.bdp$Država), 
                    aes(x=Leto, y=BDP_pc, label=Država)) + 
   geom_line(aes(group = Država,color=Država), stat = 'identity', position = 'dodge')
 
-#Graf za 10 najmanj razvitih držav
+#Graf za 6 najmanj razvitih držav
 graf_BDP_najmanjsi <- ggplot(data = BDP %>% filter(Država %in% najnizji.bdp$Država), 
                    aes(x=Leto, y=BDP_pc, label=Država)) + 
   geom_line(aes(group = Država,color=Država), stat = 'identity', position = 'dodge')
 
-#Graf za priseljevanje po spolu za Nemčijo in Švedsko 
+#Prikazal bom grafe za Nemčijo(priseljevanje) in Poljsko(odseljevanje)
+#Graf za priseljevanje po spolu za Nemčijo 
  
 graf_priseljevanja_Nem1 <- ggplot(data=imig_starost_spol %>% filter(Država=="Germany", Spol!="Total"),
                     aes(x=Leto, y=Priseljeni, fill=Spol)) + 
   geom_bar(stat="identity", position="stack")
 
-graf_priseljevanja_Swe1 <- ggplot(data=imig_starost_spol %>% filter(Država=="Sweden", Spol!="Total"),
-                                 aes(x=Leto, y=Priseljeni, fill=Spol)) + 
-  geom_bar(stat="identity", position="stack")
+
+#Graf za priseljevanje po starosti za Nemčijo 
 
 graf_priseljevanja_Nem2 <- ggplot(data=imig_starost_spol %>% filter(Država=="Germany" &Leto==2016& Starost!="Total"),
                                      aes(x="", y=Priseljeni, fill=Starost)) +
   geom_bar(stat="identity", width=1) +
   coord_polar("y", start=0) 
+
+
+najvec.izseljevanja <- emi_starost_spol %>% filter(Starost=="Total", Spol=="Total", Država %in% najnizji.bdp$Država) %>%
+  top_n(10, Izseljeni)
+
+#Graf odseljevanja po spolu za Poljsko 
+
+graf_izseljevanja_Pol1 <- ggplot(data=emi_starost_spol %>% filter(Država=="Poland",Starost=="Total"),
+                                aes(x=Leto, y=Izseljeni, fill=Spol)) + 
+  geom_bar(stat="identity", position = "dodge")
+
+#Graf odseljevanja po starosti za Poljsko 
+
+graf_izseljevanja_Pol2 <- ggplot(data=emi_starost_spol %>% filter(Država=="Poland",Spol=="Total",Starost!="Total"),
+                                aes(x=Leto, y=Izseljeni, fill=Starost)) + 
+  geom_bar(stat="identity", position = "fill")
+
+
+
+neto_tabela <- neto_tabela%>%filter(Starost=="Total", Spol=="Total", Leto!=2017)
+neto_tabela <- neto_tabela[c(-3,-4)]
+neto_bdp <- left_join(BDP,neto_tabela)
+
+#Graf odvisnosti neto priseljevanja od BDP pc 
+graf_bdp_neto <- ggplot(data=neto_bdp %>% filter(Leto==2016,Država%in%najvecji.bdp$Država | Država%in%najnizji.bdp$Država), 
+                        aes(x=BDP_pc,y=Neto)) + 
+  geom_point(aes(color=Država))
+
 
