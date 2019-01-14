73ad7a240fd9cb8a36650c06ded9aeacb0e98f2c
Žan Jernejčič <zan.jernejcic@gmail.com>
2019-01-14 19:27:47 +0100

    Narisal nekaj grafov

--- uvoz/mojuvoz.R
+++ uvoz/mojuvoz.R
@@ -5,33 +5,43 @@ library(readr)
 
 #Uvoz in ureditev tabele za BDP
 BDP <- read_csv("podatki/BDP.csv", na=":")
-BDP[,c(3,4,6)] <- NULL
-names(BDP) <- c("Leto", "Država", "BDP")
-
+BDP <- BDP[c(-3,-4,-6)]
+names(BDP) <- c("Leto", "Država", "BDP_pc")
+BDP$Država <- gsub("^Germany.*", "Germany",BDP$Država)
 
 #Uvoz in ureditev tabele za izseljevanje
 emi_naslednja <- read_csv("podatki/emi_naslednja.csv", na=":")
-emi_naslednja[,c(4:7,9)] <- list(NULL)
+emi_naslednja <- emi_naslednja[c(-4:-7,-9)]
 names(emi_naslednja) <- c("Leto", "Država", "Država izselitve", "Izseljeni")
+emi_naslednja$Država <- gsub("^Germany.*", "Germany",emi_naslednja$Država)
+emi_naslednja$`Država izselitve` <- gsub("^Germany.*", "Germany",emi_naslednja$`Država izselitve`)
 
 
 #Uvoz in ureditev tabele za izseljevanje po starosti in spolu
-emi_starost_spol <- read_csv("podatki/emi_starost_spol.csv", na=":")
-emi_starost_spol[,c(3,4,6,9)] <- list(NULL)
+emi_starost_spol <- read_csv("podatki/emi_po_starosti_in_spolu.csv", na=":")
+emi_starost_spol <- emi_starost_spol[c(-3,-4,-6,-9)]
 names(emi_starost_spol) <- c("Leto", "Država","Starost", "Spol", "Izseljeni")
+emi_starost_spol$Država <- gsub("^Germany.*", "Germany",emi_starost_spol$Država)
 
 
 #Uvoz in ureditev tabele za priseljevanje po starosti in spolu
-imig_starost_spol <- read_csv("podatki/imig_starost_spol.csv",na=":")
-imig_starost_spol[,c(3,4,6,9)] <- list(NULL)
+imig_starost_spol <- read_csv("podatki/imig_po_starosti_in_spolu.csv",na=":")
+imig_starost_spol <- imig_starost_spol[c(-3,-4,-6,-9)]
 names(imig_starost_spol) <- c("Leto", "Država", "Starost", "Spol", "Priseljeni")
 imig_starost_spol <- imig_starost_spol[!(imig_starost_spol$Država=="Former Yugoslav Republic of Macedonia, the"
 ),]
+imig_starost_spol$Država <- gsub("^Germany.*", "Germany",imig_starost_spol$Država)
+
+
+
 
 #Uvoz in ureditev tabele za priseljevanje po narodnosti
 imig_po_narodnosti <- read_csv("podatki/imig_po_narodnosti.csv", na=":")
-imig_po_narodnosti[,c(4,5,6,8)]<-list(NULL)
+imig_po_narodnosti <- imig_po_narodnosti[c(-4,-5,-6,-8)]
 names(imig_po_narodnosti) <- c("Leto", "Država", "Državljanstvo", "število")
+imig_po_narodnosti$Država <- gsub("^Germany.*", "Germany",imig_po_narodnosti$Država)
+imig_po_narodnosti$Državljanstvo <- gsub("^Germany.*", "Germany",imig_po_narodnosti$Državljanstvo)
+imig_po_narodnosti <- imig_po_narodnosti[!(imig_po_narodnosti$Državljanstvo=="Reporting country"),]
 
 
 #Moje tabele
--- vizualizacija/moja_vizualizacija.R
+++ vizualizacija/moja_vizualizacija.R
@@ -22,3 +22,35 @@ ggplot(Evropa, aes(x=long, y=lat, group=group, fill=NAME)) +
   geom_polygon() + 
   labs(title="Evropa") +
   theme(legend.position="none")
+
+
+najvecji.bdp <- BDP %>% filter(Leto == 2011) %>%
+  top_n(10, BDP_pc)
+
+najnizji.bdp <- BDP %>% filter(Leto==2011) %>% top_n(10,-1*BDP_pc)
+
+#Graf za 10 najbolj razvitih držav
+graf_BDP_najvecji <- ggplot(data = BDP %>% filter(Država %in% najvecji.bdp$Država), 
+                   aes(x=Leto, y=BDP_pc, label=Država)) + 
+  geom_line(aes(group = Država,color=Država), stat = 'identity', position = 'dodge')
+
+#Graf za 10 najmanj razvitih držav
+graf_BDP_najmanjsi <- ggplot(data = BDP %>% filter(Država %in% najnizji.bdp$Država), 
+                   aes(x=Leto, y=BDP_pc, label=Država)) + 
+  geom_line(aes(group = Država,color=Država), stat = 'identity', position = 'dodge')
+
+#Graf za priseljevanje po spolu za Nemčijo in Švedsko 
+ 
+graf_priseljevanja_Nem1 <- ggplot(data=imig_starost_spol %>% filter(Država=="Germany", Spol!="Total"),
+                    aes(x=Leto, y=Priseljeni, fill=Spol)) + 
+  geom_bar(stat="identity", position="stack")
+
+graf_priseljevanja_Swe1 <- ggplot(data=imig_starost_spol %>% filter(Država=="Sweden", Spol!="Total"),
+                                 aes(x=Leto, y=Priseljeni, fill=Spol)) + 
+  geom_bar(stat="identity", position="stack")
+
+graf_priseljevanja_Nem2 <- ggplot(data=imig_starost_spol %>% filter(Država=="Germany" &Leto==2016& Starost!="Total"),
+                                     aes(x="", y=Priseljeni, fill=Starost)) +
+  geom_bar(stat="identity", width=1) +
+  coord_polar("y", start=0) 
+
