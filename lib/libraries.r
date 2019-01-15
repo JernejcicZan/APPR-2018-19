19205153fed4481cef897fb71cb7d40548edc3dc
Žan Jernejčič <zan.jernejcic@gmail.com>
2019-01-15 11:46:30 +0100

    poprava

--- lib/libraries.r
+++ lib/libraries.r
@@ -1,8 +1,7 @@
 library(knitr)
-library(rvest)
-library(gsubfn)
+#library(rvest)
+#library(gsubfn)
 library(reshape2)
-library(shiny)
 
 # Uvozimo funkcije za pobiranje in uvoz zemljevida.
 source("lib/uvozi.zemljevid.r", encoding="UTF-8")
