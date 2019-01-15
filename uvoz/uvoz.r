1c5dd68bb855c99a8ef635a221b6f51020fb8fb5
Žan Jernejčič <zan.jernejcic@gmail.com>
2019-01-15 11:08:34 +0100

    Dokončal poročilo

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -1,54 +1,66 @@
-# 2. faza: Uvoz podatkov
+library(dplyr)
+library(tidyr)
+library(readr)
 
-sl <- locale("sl", decimal_mark=",", grouping_mark=".")
 
-# Funkcija, ki uvozi občine iz Wikipedije
-uvozi.obcine <- function() {
-  link <- "http://sl.wikipedia.org/wiki/Seznam_ob%C4%8Din_v_Sloveniji"
-  stran <- html_session(link) %>% read_html()
-  tabela <- stran %>% html_nodes(xpath="//table[@class='wikitable sortable']") %>%
-    .[[1]] %>% html_table(dec=",")
-  for (i in 1:ncol(tabela)) {
-    if (is.character(tabela[[i]])) {
-      Encoding(tabela[[i]]) <- "UTF-8"
-    }
-  }
-  colnames(tabela) <- c("obcina", "povrsina", "prebivalci", "gostota", "naselja",
-                        "ustanovitev", "pokrajina", "regija", "odcepitev")
-  tabela$obcina <- gsub("Slovenskih", "Slov.", tabela$obcina)
-  tabela$obcina[tabela$obcina == "Kanal ob Soči"] <- "Kanal"
-  tabela$obcina[tabela$obcina == "Loški potok"] <- "Loški Potok"
-  for (col in c("povrsina", "prebivalci", "gostota", "naselja", "ustanovitev")) {
-    tabela[[col]] <- parse_number(tabela[[col]], na="-", locale=sl)
-  }
-  for (col in c("obcina", "pokrajina", "regija")) {
-    tabela[[col]] <- factor(tabela[[col]])
-  }
-  return(tabela)
-}
+#Uvoz in ureditev tabele za BDP
+BDP <- read_csv("podatki/BDP.csv", na=":")
+BDP <- BDP[c(-3,-4,-6)]
+names(BDP) <- c("Leto", "Država", "BDP_pc")
+BDP$Država <- gsub("^Germany.*", "Germany",BDP$Država)
+BDP <- BDP%>%filter(Leto!=2017)
 
-# Funkcija, ki uvozi podatke iz datoteke druzine.csv
-uvozi.druzine <- function(obcine) {
-  data <- read_csv2("podatki/druzine.csv", col_names=c("obcina", 1:4),
-                    locale=locale(encoding="Windows-1250"))
-  data$obcina <- data$obcina %>% strapplyc("^([^/]*)") %>% unlist() %>%
-    strapplyc("([^ ]+)") %>% sapply(paste, collapse=" ") %>% unlist()
-  data$obcina[data$obcina == "Sveti Jurij"] <- "Sveti Jurij ob Ščavnici"
-  data <- data %>% melt(id.vars="obcina", variable.name="velikost.druzine",
-                        value.name="stevilo.druzin")
-  data$velikost.druzine <- parse_number(data$velikost.druzine)
-  data$obcina <- factor(data$obcina, levels=obcine)
-  return(data)
-}
+#Uvoz in ureditev tabele za izseljevanje
+emi_naslednja <- read_csv("podatki/emi_naslednja.csv", na=":")
+emi_naslednja <- emi_naslednja[c(-4:-7,-9)]
+names(emi_naslednja) <- c("Leto", "Država", "Država izselitve", "Izseljeni")
+emi_naslednja$Država <- gsub("^Germany.*", "Germany",emi_naslednja$Država)
+emi_naslednja$`Država izselitve` <- gsub("^Germany.*", "Germany",emi_naslednja$`Država izselitve`)
 
-# Zapišimo podatke v razpredelnico obcine
-obcine <- uvozi.obcine()
 
-# Zapišimo podatke v razpredelnico druzine.
-druzine <- uvozi.druzine(levels(obcine$obcina))
 
-# Če bi imeli več funkcij za uvoz in nekaterih npr. še ne bi
-# potrebovali v 3. fazi, bi bilo smiselno funkcije dati v svojo
-# datoteko, tukaj pa bi klicali tiste, ki jih potrebujemo v
-# 2. fazi. Seveda bi morali ustrezno datoteko uvoziti v prihodnjih
-# fazah.
+#Uvoz in ureditev tabele za izseljevanje po starosti in spolu
+emi_starost_spol <- read_csv("podatki/emi_po_starosti_in_spolu.csv", na=":")
+emi_starost_spol <- emi_starost_spol[c(-3,-4,-6,-9)]
+names(emi_starost_spol) <- c("Leto", "Država","Starost", "Spol", "Izseljeni")
+emi_starost_spol$Država <- gsub("^Germany.*", "Germany",emi_starost_spol$Država)
+emi_starost_spol$Starost <- gsub("Less than 15 years", "14 years or less", emi_starost_spol$Starost)
+emi_starost_spol$Starost <- gsub("65 years or over", "Over 65 years", emi_starost_spol$Starost)
+
+
+#Uvoz in ureditev tabele za priseljevanje po starosti in spolu
+imig_starost_spol <- read_csv("podatki/imig_po_starosti_in_spolu.csv",na=":")
+imig_starost_spol <- imig_starost_spol[c(-3,-4,-6,-9)]
+names(imig_starost_spol) <- c("Leto", "Država", "Starost", "Spol", "Priseljeni")
+imig_starost_spol <- imig_starost_spol[!(imig_starost_spol$Država=="Former Yugoslav Republic of Macedonia, the"
+),]
+imig_starost_spol$Država <- gsub("^Germany.*", "Germany",imig_starost_spol$Država)
+imig_starost_spol$Starost <- gsub("Less than 15 years", "14 years or less", imig_starost_spol$Starost)
+imig_starost_spol$Starost <- gsub("65 years or over", "Over 65 years", imig_starost_spol$Starost)
+
+
+
+#Uvoz in ureditev tabele za priseljevanje po narodnosti
+imig_po_narodnosti <- read_csv("podatki/imig_po_narodnosti.csv", na=":")
+imig_po_narodnosti <- imig_po_narodnosti[c(-4,-5,-6,-8)]
+names(imig_po_narodnosti) <- c("Leto", "Država", "Državljanstvo", "število")
+imig_po_narodnosti$Država <- gsub("^Germany.*", "Germany",imig_po_narodnosti$Država)
+imig_po_narodnosti$Državljanstvo <- gsub("^Germany.*", "Germany",imig_po_narodnosti$Državljanstvo)
+imig_po_narodnosti <- imig_po_narodnosti[!(imig_po_narodnosti$Državljanstvo=="Reporting country"),]
+
+
+neto_tabela <- left_join(imig_starost_spol, emi_starost_spol)
+neto_tabela <- neto_tabela %>% mutate(Neto = Priseljeni - Izseljeni)
+neto_tabela[,c(5,6)] <- NULL
+
+
+#Moje tabele
+BDP
+emi_naslednja
+emi_starost_spol
+imig_starost_spol
+imig_po_narodnosti
+
+
+ 
+
--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -1,13 +1,113 @@
-# 3. faza: Vizualizacija podatkov
+library(rgdal)
+library(rgeos)
+library(mosaic)
+library(maptools)
+library(reshape2)
+library(ggplot2)
+library(munsell)
 
-# Uvozimo zemljevid.
-zemljevid <- uvozi.zemljevid("http://baza.fmf.uni-lj.si/OB.zip", "OB",
-                             pot.zemljevida="OB", encoding="Windows-1250")
-levels(zemljevid$OB_UIME) <- levels(zemljevid$OB_UIME) %>%
-  { gsub("Slovenskih", "Slov.", .) } %>% { gsub("-", " - ", .) }
-zemljevid$OB_UIME <- factor(zemljevid$OB_UIME, levels=levels(obcine$obcina))
-zemljevid <- fortify(zemljevid)
 
-# Izračunamo povprečno velikost družine
-povprecja <- druzine %>% group_by(obcina) %>%
-  summarise(povprecje=sum(velikost.druzine * stevilo.druzin) / sum(stevilo.druzin))
+
+
+najvecji.bdp <- BDP %>% filter(Leto == 2011) %>%
+  top_n(8, BDP_pc)
+
+najnizji.bdp <- BDP %>% filter(Leto==2011) %>% top_n(8,-1*BDP_pc)
+
+
+#Graf BDP za 6 držav z najvišjim in najnižjim BDP-jem
+graf_BDP_najvecji <- ggplot(data = BDP %>% filter(Država %in% najvecji.bdp$Država ), 
+                   aes(x=Leto, y=BDP_pc, label=Država)) + 
+  geom_line(aes(group = Država,color=Država), stat = 'identity', position = 'dodge')
+
+graf_BDP_najmanjsi <- ggplot(data = BDP %>% filter(Država %in% najnizji.bdp$Država ), 
+                            aes(x=Leto, y=BDP_pc, label=Država)) + 
+  geom_line(aes(group = Država,color=Država), stat = 'identity', position = 'dodge')
+
+
+#Prikazal bom grafe za Nemčijo(priseljevanje) in Poljsko(odseljevanje)
+#Graf za priseljevanje po spolu za Nemčijo 
+ 
+graf_priseljevanja_Nem1 <- ggplot(data=imig_starost_spol %>% filter(Država=="Germany", Spol!="Total"),
+                    aes(x=Leto, y=Priseljeni, fill=Spol)) + 
+  geom_bar(stat="identity", position="stack") + 
+  labs(title="Priseljevanje od 2011 do 2016 po spolu za Nemčijo")
+
+
+#Graf za priseljevanje po starosti za Nemčijo 
+
+graf_priseljevanja_Nem2 <- ggplot(data=imig_starost_spol %>% filter(Država=="Germany" &Leto==2016 &Starost!="Total"),
+                                     aes(x="", y=Priseljeni, fill=Starost)) +
+  geom_bar(stat="identity", position="dodge") + 
+  labs(title="Priseljevanje v 2016 po starosti za Nemčijo")
+
+
+najvec.izseljevanja <- emi_starost_spol %>% filter(Starost=="Total", Spol=="Total", Država %in% najnizji.bdp$Država) %>%
+  top_n(10, Izseljeni)
+
+#Graf odseljevanja po spolu za Poljsko 
+
+graf_izseljevanja_Pol1 <- ggplot(data=emi_starost_spol %>% filter(Država=="Poland",Starost=="Total"),
+                                aes(x=Leto, y=Izseljeni, fill=Spol)) + 
+  geom_bar(stat="identity", position = "dodge") + 
+  labs(title = "Izseljevanje po spolu od 2011 do 2016 za Poljsko")
+
+#Graf odseljevanja po starosti za Poljsko 
+
+graf_izseljevanja_Pol2 <- ggplot(data=emi_starost_spol %>% filter(Država=="Poland",Spol=="Total",Starost!="Total",Leto==2016),
+                                aes(x=Leto, y=Izseljeni, fill=Starost)) + 
+  geom_bar(stat="identity", position = "dodge") + 
+  labs(title = "Izseljevanje po starosti v letu 2016 za Poljsko")
+
+graf_neto_pol_nem <- ggplot(data=neto_tabela %>% filter(Država %in% c("Poland","Germany")),
+                            aes(x=Leto, y=Neto, label=Država)) + 
+  geom_line(aes(group = Država,color=Država), stat = 'identity', position = 'dodge') + 
+  geom_point(aes(group = Država,color=Država), stat = 'identity', position = 'dodge') + 
+  labs(title="Neto prirastek za Nemčijo in Poljsko")
+
+neto_tabela <- neto_tabela%>%filter(Starost=="Total", Spol=="Total", Leto!=2017)
+neto_tabela <- neto_tabela[c(-3,-4)]
+neto_bdp <- left_join(BDP,neto_tabela)
+
+#Graf odvisnosti neto priseljevanja od BDP pc 
+graf_bdp_neto <- ggplot(data=neto_bdp %>% filter(Leto==2016), 
+                        aes(x=BDP_pc,y=Neto)) + 
+  geom_point(aes(color=Država)) + 
+  labs(title="Povezava med BDP in neto prirastkom priseljevanja")
+
+
+
+
+# Uvozimo zemljevid Sveta
+# source("https://raw.githubusercontent.com/jaanos/APPR-2018-19/master/lib/uvozi.zemljevid.r")
+source("lib/uvozi.zemljevid.r") #Nastavi pravo datoteko
+
+zemljevid <- uvozi.zemljevid("https://www.naturalearthdata.com/http//www.naturalearthdata.com/download/50m/cultural/ne_50m_admin_0_countries.zip",
+                             "ne_50m_admin_0_countries") %>% fortify()
+
+
+# Zemljevid sveta skrčimo na zemljevid Evrope
+Evropa <- filter(zemljevid, CONTINENT == "Europe" |NAME == "Cyprus")
+Evropa <- filter(Evropa, long < 50 & long > -30 & lat > 30 & lat < 75)
+
+ggplot(Evropa, aes(x=long, y=lat, group=group, fill=id)) + 
+  geom_polygon() + 
+  labs(title="Evropa") +
+  theme(legend.position="none")
+
+evropske_drzave <- unique(Evropa$NAME)
+evropske_drzave <- as.data.frame(evropske_drzave, stringsAsFactors=FALSE)
+names(evropske_drzave) <- "Country"
+
+priseljeni.2016 <- imig_starost_spol %>% filter(Leto==2016, Starost=="Total", Spol=="Total")
+priseljeni.2016 <- priseljeni.2016[c(-1,-3,-4)]
+names(priseljeni.2016) <- c("Country", "Priseljeni")
+
+ujemanje <- left_join(priseljeni.2016, evropske_drzave, by="Country")
+
+#Zemljevid neto prirastka za leto 2016 bolj svetle večji bolj temne negativen prirastek
+
+map <- ggplot() + geom_polygon(data=left_join(Evropa,ujemanje, by=c("NAME"="Country")),
+                               aes(x=long, y=lat, group=group, fill=Priseljeni)) +
+  ggtitle("Število priseljencev v letu 2016") + xlab("") + ylab("") +
+  guides(fill=guide_colorbar(title="Število priseljencev"))
